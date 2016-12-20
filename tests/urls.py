472cd3c61a6ac819d5e59efe68977d38e0b28d70
Jeremy Robin <jeremy@talentpair.com>
2016-12-19 21:57:14 -0800

    fix imports for 1.10

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import patterns
 from tests.test import views
 
-urlpatterns = patterns('',
-    (r'^test_api', views.test_api)
-)
+try:
+    from django.conf.urls import patterns
+    urlpatterns = patterns('',
+                           (r'^test_api', views.test_api)
+                           )
+except ImportError:
+    from django.conf.urls import url
+    urlpatterns = [
+        url(r'^test_api', views.test_api)
+    ]
